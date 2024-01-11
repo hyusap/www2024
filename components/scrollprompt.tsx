9d8ae9bd39d40a2177237c105a606a362ada9288
hyusap <paulayush@gmail.com>
2024-01-11 00:09:19 -0500

    deincentivze click

--- components/scrollprompt.tsx
+++ components/scrollprompt.tsx
@@ -4,7 +4,7 @@ export default function ScrollPrompt({ href }: { href?: string }) {
   return (
     <div className="absolute bottom-5 left-0 right-0 flex justify-center">
       <a
-        className="z-40 flex animate-bounce cursor-pointer items-center justify-center rounded-full bg-dark p-3 text-light"
+        className="z-40 flex animate-bounce items-center justify-center rounded-full bg-dark p-3 text-light"
         href={href}
         onClick={href ? undefined : (e) => window.scrollBy(0, 200)}
       >
